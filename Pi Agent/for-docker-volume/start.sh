712dbc2eb7a2a51a3c2cc1b3780298913c36dc48
Alex <alessandro.piccione.75@gmail.com>
2026-08-08 10:45:38 +0200

    remove Pi start mode choice

--- Pi Agent/for-docker-volume/start.sh
+++ Pi Agent/for-docker-volume/start.sh
@@ -33,25 +33,28 @@ start_project() {
     local model_param=""
     local llamacpp_model=$(get_llamacpp_loaded_model)
     if [[ -n "$llamacpp_model" ]]; then
-        # "Llama.cpp" is the provider used for local llama.cpp server
+        # "Llama.cpp" is the provider used for local llama.cpp server (in models.json)
         # --model.... singular, for a single model
         model_param="--model Llama.cpp/$llamacpp_model"
         echo ""
-        echo -e "Found this llama.cpp model loaded: ${YELLOW} $llamacpp_model ${NC}"
+        echo -e "Found this llama.cpp model running: ${YELLOW} $llamacpp_model ${NC}"
     fi
 
     # Launch Pi Agent
-    echo -e "\e[34m-------------------\e[0m"
-    PS3=$'\e[34mSelect an option: \e[0m'
-    select choice in Continue Resume New ; do
-        # use exec so when the host shell close the pi process is killed
-        case "$choice" in
-            Continue)  exec pi $model_param --continue ;;
-            Resume)    exec pi $model_param --resume ;;
-            New)       exec pi $model_param --name $(date +%y.%m.%d) ;; # YY.MM.DD
-            #No-session|*) exec pi $model_param --no-session ;;
-        esac
-    done
+    exec pi $model_param --continue ;;
+
+    # user choice
+    #echo -e "\e[34m-------------------\e[0m"
+    #PS3=$'\e[34mSelect an option: \e[0m'
+    #select choice in Continue Resume New ; do
+    #    # use exec so when the host shell close the pi process is killed
+    #    case "$choice" in
+    #        Continue)  exec pi $model_param --continue ;;
+    #        Resume)    exec pi $model_param --resume ;;
+    #        New)       exec pi $model_param --name $(date +%y.%m.%d) ;; # YY.MM.DD
+    #        #No-session|*) exec pi $model_param --no-session ;;
+    #    esac
+    #done
             
     echo -e "Pi Agent session closed. Bye!"
 }
